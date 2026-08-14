cf1f85ee7b693d58e63bccd97bb7371aabcfa48f
ayegalapati <abhiyegalapati@gmail.com>
2026-08-13 22:56:43 -0700

    Finished Carousel Implementation

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,21 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick"; 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"; 
 
 const HotCollections = () => {
-
   const [users, setUsers] = useState([]);
 
   async function getUsers() {
-    const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+    );
     setUsers(data);
   }
 
   useEffect(() => {
     getUsers();
   }, []);
+
+  // Updated carousel settings
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4, 
+    slidesToScroll: 1,
+    autoplay: false, // Changed to false: Stops automatic movement
+    arrows: true,    // Keeps the left and right arrows active
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: { slidesToShow: 3 }
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2 }
+      },
+      {
+        breakpoint: 576,
+        settings: { slidesToShow: 1 }
+      }
+    ]
+  };
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -27,29 +54,34 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {users.map((user) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={user.id}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={user.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+          
+          <div className="col-lg-12">
+            <Slider {...sliderSettings}>
+              {users.map((user) => (
+                <div className="px-3" key={user.id}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img src={user.nftImage} className="lazy img-fluid" alt="" />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img className="lazy pp-coll" src={user.authorImage} alt="" />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{user.title}</h4>
+                      </Link>
+                      <span>ERC-{user.code}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={user.authorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{user.title}</h4>
-                  </Link>
-                  <span>ERC-{user.code}</span>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
